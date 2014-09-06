8307003d4e4f2b66eb3c2f53acf0fb345f14a765
Monica Olinescu <monica.olinescu@gmail.com>
2014-09-06 11:52:19 -0700

    fix(validate): Add tests for validator #2

--- test/angular-uuid4.spec.js
+++ test/angular-uuid4.spec.js
@@ -13,5 +13,21 @@ describe('uuid4', function() {
       expect(uuidService.generate()).toMatch(/[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}/)
     });
 
+    it('should validate a lower cased uuid', function(){
+      // uuid taken from RFC http://www.ietf.org/rfc/rfc4122.txt
+      expect(uuidService.validate('f81d4fae-7dec-11d0-a765-00a0c91e6bf6')).toEqual(true);
+    });
+
+    it('should validate an uppper cased uuid', function(){
+      expect(uuidService.validate('F81D4FAE-7DEC-11D0-A765-00A0C91E6BF6')).toEqual(true);
+    });
+
+    it('should identify an invalid uuid', function(){
+      expect(uuidService.validate('invalid')).toEqual(false);
+    });
+
+    it('should identify an undefined uuid', function(){
+      expect(uuidService.validate(undefined)).toEqual(false);
+    });
   });
 });
